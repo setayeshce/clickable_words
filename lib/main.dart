a4942525f5f01b206b02548dbb0d3c6e173055e3
setayeshce <setayesh.hoseiini79@gmaile.com>
2023-08-10 10:15:11 +0330

    clickable words and phrase  in sentence

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyClickableText(),
     );
   }
 }
 
-class MyClickableText extends StatelessWidget {
+class MyClickableText extends StatefulWidget {
+  @override
+  State<MyClickableText> createState() => _MyClickableTextState();
+}
+
+class _MyClickableTextState extends State<MyClickableText> {
+  var phraseText = '';
+
+  var wordText = '';
+
+  void _onPhraseTap(String phrase) {
+   setState(() {
+     phraseText = phrase;
+   });
+    print('Clicked on phrase: $phrase');
+  }
+
   void _onWordTap(String word) {
-    print('Clicked on: $word');
+    setState(() {
+      wordText = word;
+    });
+    print('Clicked on word: $word');
   }
 
   @override
   Widget build(BuildContext context) {
     String text = """
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+   It was popularised in the 1960s with the release of Letraset sheets containing
+    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 """;
 
-    List<String> words = text.split(RegExp('\\s+'));
-    return Scaffold(
-      body: Center(
-        child: RichText(textAlign: TextAlign.center,
-          text: TextSpan(
-            children: words.map((word) {
-              return TextSpan(
+    List<String> phrases = [
+      "simply dummy",
+      "typesetting industry",
+      "printer took",
+      "specimen book",
+      "five centuries",
+      "including versions",
+      "Lorem Ipsum"
+    ];
+
+    List<TextSpan> textSpans = [];
+    String tempText = text;
+
+    for (String phrase in phrases) {
+      while (tempText.contains(phrase)) {
+        var splitText = tempText.split(phrase);
+        splitText[0].split(RegExp(r'\s+')).forEach((word) {
+          if(word.isNotEmpty){
+            textSpans.add(
+              TextSpan(
                 text: '$word ',
-                style: TextStyle(fontSize: 18.0, color: Colors.blue),
+                style: TextStyle(fontSize: 18.0, color: Colors.black),
                 recognizer: TapGestureRecognizer()
                   ..onTap = () {
                     _onWordTap(word);
                   },
-              );
-            }).toList(),
+              ),
+            );
+          }
+        });
+
+        textSpans.add(
+          TextSpan(
+            text: phrase,
+            style: const TextStyle(
+              fontSize: 18.0,
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                _onPhraseTap(phrase);
+              },
+          ),
+        );
+
+        tempText = splitText.length > 1 ? splitText.sublist(1).join(phrase) : "";
+      }
+    }
+
+    tempText.split(RegExp(r'\s+')).forEach((word) {
+      if(word.isNotEmpty){
+        textSpans.add(
+          TextSpan(
+            text: '$word ',
+            style: TextStyle(fontSize: 18.0, color: Colors.black),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                _onWordTap(word);
+              },
+          ),
+        );
+      }
+    });
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50 , vertical: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("phrase : $phraseText" , style: TextStyle(fontSize: 18.0, color: Colors.orange),),
+              const SizedBox(height: 20,),
+              Text("words : $wordText" ,   style: TextStyle(fontSize: 18.0, color: Colors.orange),),
+              const SizedBox(height: 20,),
+              Center(
+                child: RichText(
+                  textAlign: TextAlign.left,
+                  text: TextSpan(children: textSpans, style: const TextStyle(color: Colors.black, fontSize: 18.0 ,
+                      height: 1.5
+                  )),
+                ),
+              ),
+            ],
           ),
         ),
       ),
